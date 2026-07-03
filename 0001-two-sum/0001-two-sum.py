64eb064cf25103aa1644705eca413094e74b5b1d
Tarun Munjeti <73815674+Hubbousers@users.noreply.github.com>
2026-07-03 21:21:24 +0530

    Time: 0 ms (100%), Space: 20.5 MB (40.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,24 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        indexes = []
-        for i in range(0, len(nums)):
-            x = target - nums[i]
-            remaining = nums[i+1:]
-            if(x in remaining):
-                indexes.append(i)
-                r_index = remaining.index(x) + (i+1)
-                indexes.append(r_index)
-                break
-        return indexes
+
+        #Solution 1:  Time = O(n) and space = O(2n)
+        # indexes = []
+        # for i in range(0, len(nums)):
+        #     x = target - nums[i]
+        #     remaining = nums[i+1:]
+        #     if(x in remaining):
+        #         indexes.append(i)
+        #         r_index = remaining.index(x) + (i+1)
+        #         indexes.append(r_index)
+        #         break
+        # return indexes
+        #Solution 2
+        dictionary = {}
+        for i in range(len(nums)):
+            search_element = target - nums[i]
+            if search_element in dictionary:
+                return [dictionary[search_element], i]
+            dictionary[nums[i]] = i
         
 
 # Synced seamlessly with LeetHub Pro

e5fd334aa06cff944cacc897c90d8be52b1a53f5
Tarun Munjeti <73815674+Hubbousers@users.noreply.github.com>
2026-08-04 09:59:53 +0530

    Time: 351 ms (13.11%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(1)

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,10 @@
-select MAX(case when dr = 2 then salary end) as SecondHighestSalary  from
-(
-select salary, DENSE_RANK() OVER (order by salary desc) as dr from Employee where salary is not null
-) t;
+-- select MAX(case when dr = 2 then salary end) as SecondHighestSalary  from
+-- (
+-- select salary, DENSE_RANK() OVER (order by salary desc) as dr from Employee where salary is not null
+-- ) t;
+
+# Write your MySQL query statement below
+select max(salary) as SecondHighestSalary from Employee where salary not in (select max(salary) from Employee);
 
 -- Synced seamlessly with LeetHub Pro
 -- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
